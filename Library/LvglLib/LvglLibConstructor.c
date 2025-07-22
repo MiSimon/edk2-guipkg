db6eccf525dac388182156603a5b26b9474696c5
Michael Simon <michael.simon.at@gmail.com>
2025-07-22 16:59:42 +0200

    Changes required by LVGL v9.3.0
    - Changed file list
    - LV_FS_DEFAULT_DRIVE_LETTER to LV_FS_DEFAULT_DRIVER_LETTER

--- Library/LvglLib/LvglLibConstructor.c
+++ Library/LvglLib/LvglLibConstructor.c
@@ -7,10 +7,10 @@ LvglLibConstructor (
   IN EFI_HANDLE        ImageHandle,
   IN EFI_SYSTEM_TABLE  *SystemTable)
 {
-	lv_uefi_init(ImageHandle, SystemTable);
+	  lv_uefi_init(ImageHandle, SystemTable);
     lv_init();
 	
     if(!lv_is_initialized()) return EFI_NOT_READY;
 	
-	return EFI_SUCCESS;
+	  return EFI_SUCCESS;
 }
